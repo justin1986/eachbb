3e05836087540175d882c54192d6747e315055bb
xiangxiangxx <380252551@qq.com>
2010-06-09 12:28:20 +0800

    xx

---
+++ test_end.php
@@ -0,0 +1,310 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html>
+<head>
+<title>课程</title>
+<link href="./css/test_end.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="ibody">
+	<div id="fbody">
+		<div id="top_login">
+			<div id="tl_l"></div>
+			<div id="tl_r"></div>
+		</div>
+		<div id="top_menu">
+			<div id="menu_pg">
+				<div id="menu_left"></div>
+				<div id="menu_center">
+					<div id="menu_ct">
+						<div id="menu_ctt">
+							<input type="button" class="t_first" value="网站首页">
+							<a href="#">
+							<div class="me_a" style="background:url(images/bbs/mo_pg.jpg) no-repeat;"><font class="son">特色</font><font class="bbs">测评</font></div>
+							</a>
+							<div class="me_h"></div>
+							<a href="#">
+							<div class="me_b">早教课程</div>
+							</a>
+							<div class="me_h"></div>
+							<a href="#">
+							<div class="me_b">妈妈助手</div>
+							</a>
+							<div class="me_h"></div>
+							<a href="#">
+							<div class="me_b">我家小院子</div>
+							</a>
+							<div class="me_h"></div>
+							<a href="#">
+							<div class="me_b">我的宝宝</div>
+							</a>
+							<div class="me_h"></div>
+							<a href="#">
+							<div class="me_b">亲子论坛</div>
+							</a>
+							<div class="me_h"></div>
+							<a href="#">
+							<div class="me_b">育儿咨询</div>
+							</a>
+							<div class="me_h"></div>
+							<a href="#">
+							<div class="me_b" style="width:85px;">关于网趣宝宝</div>
+							</a>
+							<div class="welcom">欢迎,<font>123</font></div>
+							<div class="exit"><a href="#" style="color:#000;">退出</a></div>
+							<div class="info">消息<a href="#" style="color:#FE843A">(1)</a></div>
+						</div>
+					</div>
+					<div id="menu_ctb">
+						<input type="button" id="t_first" value="我的网趣">
+						<a href="#">
+						<div class="me_aa">网趣宝宝首页</div>
+						</a>
+						<div class="me_hh"></div>
+						<a href="#">
+						<div class="me_bb">用户测评报告</div>
+						</a>
+						<div class="me_hh"></div>
+						<a href="#">
+						<div class="me_bb">用户课程订购</div>
+						</a>
+						<div class="me_hh"></div>
+						<a href="#">
+						<div class="me_bb">重要信息提示</div>
+						</a>
+						<div class="me_hh"></div>
+						<a href="#">
+						<div class="me_bb">我家小院子</div>
+						</a>
+						<div class="me_hh"></div>
+						<a href="#">
+						<div class="me_bb" >管理设置</div>
+						</a>
+						<input type="text" id="me_in">
+						<input type="button" id="me_btn">
+					</div>
+				</div>
+				<div id="menu_right"></div>
+			</div>
+		</div>
+		<div id="content">
+			<div id="c_l">
+				<div id="l_pho">
+					<div id="l_p_t"></div>
+					<div id="lp">
+						<div id="lp_c">
+							<div id="lp_t">个人信息管理</div>
+							<div id="lp_p">
+								<div id="lp_l"><img src="images/class/l_peo.jpg"></div>
+								<div id="lp_word">司法撒旦发射发声法</div>
+							</div>
+							<div id="lp_cc">
+								<div id="lopul">
+									<ul id="lp_ul">
+										<li><a href="#">您有<font>0</font>条评论</a></li>
+										<li><a href="#">我的博客</a></li>
+										<li><a href="#">宝宝最新测试报告</a></li>
+										<li><a href="#">宝宝本期课程</a></li>
+										<li><a href="#">宝宝下期课程提示</a></li>
+									</ul>
+								</div>
+							</div>
+							<div id="lp_ck"></div>
+						</div>
+					</div>
+					<div id="l_p_b"></div>
+				</div>
+				
+				<div id="l_look">
+					<a href="#">网趣宝贝<font>意见反馈</a>&nbsp;&nbsp;</font>
+				</div>
+				
+				<div class="hlc_t"></div>
+				<div class="hlc_b">
+					<div class="hlcb_pg">
+						<div class="hlct_t">热门课程排行榜</div>
+						<img src="images/helper/lb_hd.jpg"> </div>
+					<div class="hlcb_z">
+						<div class="hlcb_l">
+							<div class="r">1</div>
+						</div>
+						<div class="hlcb_r">
+							<div class="hlcb_t">按时法十分</div>
+							<div class="hlcb_t">上传者：哈哈</div>
+							<div class="hlcb_t"><font>下载次数：</font>1232</div>
+						</div>
+					</div>
+					<div class="hlcb_z">
+						<div class="hlcb_l">
+							<div class="r">2</div>
+						</div>
+						<div class="hlcb_r">
+							<div class="hlcb_t">按时法十分</div>
+							<div class="hlcb_t">上传者：哈哈</div>
+							<div class="hlcb_t"><font>下载次数：</font>1232</div>
+						</div>
+					</div>
+					<div class="hlcb_z">
+						<div class="hlcb_l">
+							<div class="r">3</div>
+						</div>
+						<div class="hlcb_r">
+							<div class="hlcb_t">按时法十分</div>
+							<div class="hlcb_t">上传者：哈哈</div>
+							<div class="hlcb_t"><font>下载次数：</font>1232</div>
+						</div>
+					</div>
+				</div>
+				<div class="hlc_bb"></div>
+				<div id="l_f">
+					<div id="lf_l">我的<font>好友</font></div>
+					<div id="lf_c">（<font>345</font>）</div>
+					<div id="lf_r">More >></div>
+				</div>
+				<div id="pic_r">
+					<?php for($i=0;$i<6;$i++){ ?>
+					<div class="ppg">
+						<div class="pic_pg">
+							<div class="pic_img"><img src="images/yard/pho.jpg"></div>
+						</div>
+						<div class="ppg_w">safsadfas</div>
+					</div>
+					<?php 
+																				}?>
+				</div>
+				<div id="pg_a"></div>
+			</div>
+			<div id="c_r">
+				<div id="cr_flash">
+					<div id="crf_l">falsh</div>
+					<div id="crf_r">
+						<div id="crf_t">特色评价<font>特色介绍</font></div>
+						<div id="crf_c">
+							<div id="crf_ti"><img src="images/test/3.jpg"></div>
+							<div id="crg_tt"><a href="#">发生在妈妈视线内的九种意</a></div>
+							发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意发生在妈妈视线内的九种意 </div>
+						<div id="crf_d"> <a href="#" style="color:#0086F8;">
+							<div class="cr_num" id="cr_num">1</div>
+							</a> <a href="#">
+							<div class="cr_num">2</div>
+							</a> <a href="#">
+							<div class="cr_num">3</div>
+							</a> <a href="#">
+							<div class="cr_num">4</div>
+							</a> </div>
+					</div>
+				</div>
+				<div id="cr_b">
+					<div id="crb_t"> 
+						<div class="crb_value">
+							<div class="crb_tt"></div>
+							<div class="crb_cc" ><a href="#">大动作</a></div>
+						</div>
+						<div class="crb_hh"></div>
+						<div class="crb_value">
+							<div class="crb_tt"></div>
+							<div class="crb_cc"><a href="#">精细动作</a></div>
+						</div>
+						<div class="crb_hh"></div>
+						<div class="crb_value">
+							<div class="crb_tt"  ></div>
+							<div class="crb_cc" ><a href="#">语言</a></div>
+						</div>
+						<div class="crb_hh" ></div>
+						<div class="crb_value">
+							<div class="crb_tt"></div>
+							<div class="crb_cc"><a href="#">认识</a></div>
+						</div>
+						<div class="crb_hh"></div>
+						<div class="crb_vv">
+							<div class="crb_ttt"></div>
+							<div class="crb_ccc"  id="crbt" ><a href="#">社会活动和行为规范</a></div>
+						</div>
+						<div id="cr_hr">
+							<div class="crb_g">
+								<div id="crbi_v"><a href="#">更多</a></div>
+								<div class="crb_img"></div>
+							</div>
+						</div>
+					</div>
+					<div id="crb_c">
+							<div id="crbc_a">
+								<a href="#">
+									<img src="images/test/tr_a.jpg">
+								</a>
+							</div>
+							<div id="crbc_z">
+								<?php for($i=0;$i<5;$i++){?>
+								<div class="crbc_ii">
+									<a href="#"><img src="images/test/as.jpg"></a>
+									<div class="crbci_t">
+										来自 <a href="#">前秘密</a>
+									</div>
+									<div class="crbci_c">
+										2010-05-27
+									</div>
+								</div>
+								<?php 
+								}?>
+							</div>
+					</div>
+					
+					</div>
+			<div id="crc"></div>
+			<div id="crd">
+				<div class="crd_z" id="crd_z"><img src="images/test/43.jpg"><div class="crd_f"><a href="#">宝宝测评案例</a></div></div>
+				<div class="crd_z"><img src="images/test/43.jpg"><div class="crd_f"><a href="#">宝宝测评案例</a></div></div>
+				<div class="crd_z"><img src="images/test/43.jpg"><div class="crd_f"><a href="#">宝宝测评案例</a></div></div>
+			</div>
+			<div id="cre">
+				<a href="#">
+					<img src="images/test/tc_b.jpg">
+					<div id="crb_h">免费课程订购等你来拿，快快行动哟！！！</div>
+				</a>
+			</div>
+			<div id="crf">
+				<div class="cf_z">
+					<div class="cfz_t">
+						<font>父母养育测试</font>
+						<a href="#">更多>></a>
+					</div>
+					<div class="cfz_cz">
+						<ul>
+							<?php
+							 for($i=0;$i<7;$i++){ ?>
+							<li>
+								<div class="frzcd"></div>
+								<div class="frzc_c"><a href="#"><font>[知识榜单]</font> 好啊好哦啊粉色的发生</a></div>
+							</li>
+							<?php
+							}?>
+						</ul>
+					</div>		
+				</div>
+				<div class="cf_z" id="cf_z">
+					<div class="cfz_t">
+						<font>父母养育测试</font>
+						<a href="#">更多>></a>
+					</div>
+					<div class="cfz_cz">
+						<ul>
+							<?php
+							 for($i=0;$i<7;$i++){ ?>
+							<li>
+								<div class="frzcd"></div>
+								<div class="frzc_c"><a href="#"><font>[知识榜单]</font> 好啊好哦啊粉色的发生</a></div>
+							</li>
+							<?php
+							}?>
+						</ul>
+					</div>		
+				</div>
+			</div>
+			</div>
+		</div>
+		<div id="bg_hr"></div>
+		<div id="bottom">关于我们 - 加入我们 - 友情链接 - 联系我们 - 服务条款 - 隐私保护 - 网站地图</div>
+		<div id="bottom_b">哈哈少儿旗下网站  Copyright © 1997-2010 HAHA.smg.com All Rights Reserved.</div>
+	</div>
+</div>
+</body>
+</html>
