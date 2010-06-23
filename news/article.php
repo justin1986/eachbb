7d8da5dc99387427f4ea9485800d914d756458a6
xiangxiangxx <380252551@qq.com>
2010-06-23 10:54:03 +0800

    xx

--- news/article.php
+++ news/article.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <?php
 	include_once('../frame.php');
-	$id =intval(trim($_REQUEST['id']));
+	$id =1033;#intval(trim($_REQUEST['id']));
 	if(empty($id)){
 		#redirect('error.html');
 		#die();
