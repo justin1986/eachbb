f263e0fffdbdc3fc6fb769c5d4dc306a1d169fb0
xiangxiangxx <380252551@qq.com>
2010-06-25 15:28:32 +0800

    xx

--- news/news.php
+++ news/news.php
@@ -72,8 +72,8 @@
 				<?php  print_news_fck_pages2($content,'article.php?id='.$id,'page');?>
 			</div>
 			<?php 
-				$sql="SELECT id,resource_id,title,nick_name,comment,created_at FROM eb_comment e where resource_type='news' and resource_id ={$id} order by created_at desc";
-				$list_news=$db->paginate($sql,7);
+				$sql = "SELECT id,resource_id,title,nick_name,comment,created_at FROM eb_comment e where resource_type='news' and resource_id ={$id} order by created_at desc";
+				$list_news = $db->paginate($sql,7);
 			?>
 			<div id="critique">
 				<div id="c_l">读者评论<a href="#">(共<?php echo $page_record_count; ?>条)</a></div>
