1b20949f6cb4b57dbbe6266f88630a6e23a57fe9
xiangxiangxx <380252551@qq.com>
2010-06-28 15:06:25 +0800

    xx

--- javascript/news/news.js
+++ javascript/news/news.js
@@ -2,11 +2,6 @@ $(function(){
 	$.post('/news/get_comments.ajax.php?id=' + $('#newsid').val(),function(news){
 		$("#res").html(news);
 	});
+	
 });
 
-$(".up").live('click',function(e){
-	$.post('/news/ajax.post.php',{"type":"up"},function(type){
-	$(".up font").html(type);
-	});
-	e.preventDefault();
-});
