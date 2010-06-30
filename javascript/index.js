d033f9833c27fde0bca27aceb47b5a8437dbe7c2
sauger <sauger@163.com>
2010-06-30 11:49:22 +0800

    fix bugs

--- javascript/index.js
+++ javascript/index.js
@@ -8,7 +8,7 @@ function search_news(){
 	window.location.href = "/news/search.php?key=" + text;
 }
 $(function(){
-	$('#login_l').click(function(e){
+	$('#login_l').live('click',function(e){
 		e.preventDefault();
 		$.post('/login/ajax.post.php?op=login&name='+ encodeURI($('#login_name').val()) + '&password=' +encodeURI($('#login_password').val()),function(data){
 			$('#test_right').load('/login/ajax.post.php?op=load_login_status_box');
