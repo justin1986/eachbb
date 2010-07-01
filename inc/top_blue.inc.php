88e2386c0ee5d40fd8196df12f84218c92c53501
xiangxiangxx <380252551@qq.com>
2010-07-01 10:36:12 +0800

    xx

--- inc/top_blue.inc.php
+++ inc/top_blue.inc.php
@@ -8,68 +8,40 @@
 				<div id="menu_center">
 					<div id="menu_ct">
 						<div id="menu_ctt">
-							<input type="button" class="t_first" value="网站首页">
-							<a href="#">
-							<div class="me_a">特色测评</div>
-							</a>
+							<div  class="t_first">网站首页</div>
+							<div class="me_b" style="margin-left:15px; "><a href="#">特色测评</a></div>
 							<div class="me_h"></div>
-							<a href="#">
-							<div class="me_b" style="background:url(images/bbs/mo_pg.jpg) no-repeat;"><font class="son">早教</font><font class="bbs">课程</font></div>
-							</a>
+							<div class="me_b" style="background:url(images/bbs/mo_pg.jpg) no-repeat;"><a href="#"><font class="son">早教</font><font class="bbs">课程</font></a></div>
 							<div class="me_h"></div>
-							<a href="#">
-							<div class="me_b">妈妈助手</div>
-							</a>
+							<div class="me_b"><a href="#">妈妈助手	</a></div>
 							<div class="me_h"></div>
-							<a href="#">
-							<div class="me_b">我家小院子</div>
-							</a>
+							<div class="me_b"><a href="#">我家小院子</a></div>
 							<div class="me_h"></div>
-							<a href="#">
-							<div class="me_b">我的宝宝</div>
-							</a>
+							<div class="me_b"><a href="#">我的宝宝</a></div>
 							<div class="me_h"></div>
-							<a href="#">
-							<div class="me_b">亲子论坛</div>
-							</a>
+							<div class="me_b"><a href="#">亲子论坛</a></div>
 							<div class="me_h"></div>
-							<a href="#">
-							<div class="me_b">育儿咨询</div>
-							</a>
+							<div class="me_b"><a href="#">育儿咨询</a></div>
 							<div class="me_h"></div>
-							<a href="#">
-							<div class="me_b" style="width:85px;">关于网趣宝宝</div>
-							</a>
+							<div class="me_b" style="width:85px;"><a href="#">关于网趣宝宝</a></div>
 							<div class="welcom">欢迎,<font>123</font></div>
 							<div class="exit"><a href="#" style="color:#000;">退出</a></div>
 							<div class="info">消息<a href="#" style="color:#FE843A">(1)</a></div>
 						</div>
 					</div>
 					<div id="menu_ctb">
-						<input type="button" id="t_first" value="我的网趣">
-						<a href="#">
-						<div class="me_aa">网趣宝宝首页</div>
-						</a>
+						<div  id="t_first">我的网趣</div>
+						<div class="me_aa"><a href="#">网趣宝宝首页</a></div>
 						<div class="me_hh"></div>
-						<a href="#">
-						<div class="me_bb">用户测评报告</div>
-						</a>
+						<div class="me_bb"><a href="#">用户测评报告</a></div>
 						<div class="me_hh"></div>
-						<a href="#">
-						<div class="me_bb">用户课程订购</div>
-						</a>
+						<div class="me_bb"><a href="#">用户课程订购</a></div>
 						<div class="me_hh"></div>
-						<a href="#">
-						<div class="me_bb">重要信息提示</div>
-						</a>
+						<div class="me_bb"><a href="#">重要信息提示</a></div>
 						<div class="me_hh"></div>
-						<a href="#">
-						<div class="me_bb">我家小院子</div>
-						</a>
+						<div class="me_bb"><a href="#">我家小院子</a></div>
 						<div class="me_hh"></div>
-						<a href="#">
-						<div class="me_bb" >管理设置</div>
-						</a>
+						<div class="me_bb" ><a href="#">管理设置</a></div>
 						<input type="text" id="me_in">
 						<input type="button" id="me_btn">
 					</div>
