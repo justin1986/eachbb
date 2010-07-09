779226254ef2b357b3fe3d8ee30ecb2072fe4e94
sauger <sauger@163.com>
2010-07-09 14:52:46 +0800

    fix bug

--- assistant/_assistant_top.php
+++ assistant/_assistant_top.php
@@ -54,15 +54,15 @@
 		<div id="top2_t">
 			<div id="tt_l"></div>
 			<div id="tt_c">
-				<div class="ttc_a"><a href="?age=-2">准备怀孕</a></div>
+				<div class="ttc_a"><a href="index.php?age=-2">准备怀孕</a></div>
 				<div class="ttc_hr"></div>
-				<div class="ttc_a"><a href="?age=-1">怀孕期</a></div>
+				<div class="ttc_a"><a href="index.php?age=-1">怀孕期</a></div>
 				<div class="ttc_hr"></div>
-				<div class="ttc_a"><a href="?age=1">0～1岁</a></div>
+				<div class="ttc_a"><a href="index.php?age=1">0～1岁</a></div>
 				<div class="ttc_hr"></div>
-				<div class="ttc_a"><a href="?age=2">1～2岁</a></div>
+				<div class="ttc_a"><a href="index.php?age=2">1～2岁</a></div>
 				<div class="ttc_hr"></div>
-				<div class="ttc_a"><a href="?age=3">2～3岁</a></div>
+				<div class="ttc_a"><a href="index.php?age=3">2～3岁</a></div>
 				<a href="#">
 				<img id="ttc_f" src="/images/helper/t_1.jpg"/>
 				</a>
