51ad091324680b50ea5ffc6643e101c328fb5322
justin <zhanghao@xun-ao.com>
2010-07-13 14:48:53 +0800

    member

--- member/member.php
+++ member/member.php
@@ -5,9 +5,10 @@
 	<meta http-equiv=Content-Language content=zh-CN>
 	<title>网趣宝贝-用户中心</title>
 	<?php 
-		include_once dirname(__FILE__).'/frame.php';
+		include_once dirname(__FILE__).'/../frame.php';
 		css_include_tag('member');
 		use_jquery();
+		js_include_tag('member');
 	?>
 </head>
 <?php
@@ -17,6 +18,7 @@
 	}
 	$member = new table_class('eb_member');
 	$member->find($id);
+	$db = get_db();
 ?>
 <body>
 <div id="top_login">
@@ -103,6 +105,7 @@
 	</div>
 	<div id=ibody_right>
 		<div class=r_title>基本<span>信息</span></div>
+		<form>
 		<table class=r_table>
 			<tr>
 				<td class=td1>昵称：</td>
@@ -114,26 +117,26 @@
 			</tr>
 			<tr>
 				<td class=td1>性别：</td>
-				<td class=td2><input class="radio" type="radio" name="gender" <?php if($member->gender==1){?>checked=checked<?php }?> value="1" />
+				<td class=td2><input class="radio" type="radio" name='gender' <?php if($member->gender==1){?>checked=checked<?php }?> value="1">
 					男
-					<input class="radio" type="radio" name="gender" value="2" <?php if($member->gender==2){?>checked=checked<?php }?>/>
+					<input class="radio" type="radio" value="2"  name='gender' <?php if($member->gender==2){?>checked=checked<?php }?>>
 					女</td>
 			</tr>
 			<tr>
 				<td class=td1>固定电话：</td>
-				<td class=td2><input name='fix_phone' value="<?php echo $member->fix_phone;?>" type="text"/></td>
+				<td class=td2><input name='fix_phone' maxlength="13" value="<?php echo $member->fix_phone;?>" type="text"/></td>
 			</tr>
 			<tr>
 				<td class=td1>联系手机：</td>
-				<td class=td2><input type="text" name='phone' value="<?php echo $member->phone;?>" /></td>
+				<td class=td2><input type="text" name='phone' maxlength="11" value="<?php echo $member->phone;?>" /></td>
 			</tr>
 			<tr>
 				<td class=td1>身份证号：</td>
-				<td class=td2><input type="text" name='id_num' value="<?php echo $member->id_num;?>" /></td>
+				<td class=td2><input type="text" name='id_num' maxlength="18" value="<?php echo $member->id_num;?>" /></td>
 			</tr>
 			<tr>
 				<td class=td1>教育程度：</td>
-				<td class=td2><select name='education' id='education'>
+				<td class=td2><select name='education'>
 						<option value=''>请选择</option>
 						<option value='高中/中专'>高中/中专</option>
 						<option value='大学本科/大学专科'>大学本科/大学专科</option>
@@ -141,10 +144,10 @@
 						<option value='博士'>博士</option>
 					</select></td>
 			</tr>
-			<script>$("#education").val('<?php echo $member->education;?>')</script>
+			<script>$("[name=education]").val('<?php echo $member->education;?>')</script>
 			<tr>
 				<td class=td1>行业：</td>
-				<td class=td2><select name='industry' id="industry">
+				<td class=td2><select name='industry'>
 						  <option value=''>请选择</option>
 						  <option value="1.制造业">1.制造业</option>
 	                      <option value="2.进出口贸易">2.进出口贸易</option>
@@ -168,10 +171,10 @@
 	                      <option value="20.其他">20.其他</option>
 					</select></td>
 			</tr>
-			<script>$("#industry").val('<?php echo $member->industry;?>')</script>
+			<script>$("[name=industry]").val('<?php echo $member->industry;?>')</script>
 			<tr>
 				<td class=td1>家庭月收入：</td>
-				<td class=td2><select name='income' id='income'>
+				<td class=td2><select name='income'>
 						<option value=''>请选择</option>
 						<option value='1000元以下'>1000元以下</option>
 						<option value='1000-3000元'>1000-3000元</option>
@@ -181,49 +184,71 @@
 						<option value='10000元以上'>10000元以上</option>
 					</select></td>
 			</tr>
-			<script>$("#income").val('<?php echo $member->income;?>')</script>
+			<script>$("[name=income]").val('<?php echo $member->income;?>')</script>
 		</table>
 		<div class=r_title>会员<span>信息</span></div>
 		<table class=r_table>
 			<tr>
-				<td class=td1><span>*</span> 注册手机：</td>
-				<td class=td2><input type="text"/></td>
-			</tr>
-			<tr>
 				<td class=td1><span>*</span> 真实姓名：</td>
-				<td class=td2><input type="text"/></td>
+				<td class=td2><input name="true_name" maxlength="6" value="<?php echo $member->true_name;?>" type="text"/></td>
 			</tr>
+			<?php 
+				$address = $member->address;
+				$address = explode('-',$address);
+			?>
 			<tr>
 				<td class=td1><span>*</span> 地址：</td>
-				<td class=td2><select>
-						<option>请选择</option>
+				<td class=td2><select name="province">
+						<option value=''>请选择</option>
+						<?php
+							 $province = $db->query("select province from phpcms_city group by province order by cityid");
+							 foreach($province as $v){
+						?>
+						<option value='<?php echo $v->province;?>'><?php echo $v->province;?></option>
+						<?php }?>
 					</select>
-					<select>
-						<option>请选择</option>
+					<script>$("[name=province]").val('<?php echo $address[0];?>')</script>
+					<select name="city">
+						<option value=''>请选择</option>
 					</select>
-					<select>
-						<option>请选择</option>
-					</select>
+					<script>show_city('<?php echo $address[1];?>');</script>
 					<br>
-					<input type="text"/></td>
+					<input name="address" maxlength="30" value="<?php echo $address[2];?>" type="text"/></td>
 			</tr>
 			<tr>
 				<td class=td1><span>*</span> 邮政编码：</td>
-				<td class=td2><input type="text"/></td>
+				<td class=td2><input name="zip" maxlength="6" value="<?php echo $member->zip;?>" type="text"/></td>
 			</tr>
 			<tr>
+			<?php 
+				$birthday = substr($member->birthday,0,10);
+				$birthday = explode('-',$birthday);
+				if($birthday[1]<10)$birthday[1] = substr($birthday[1],1,1);
+				if($birthday[2]<10)$birthday[2] = substr($birthday[2],1,1);
+			?>
 				<td class=td1><span>*</span> 您的生日：</td>
-				<td class=td2><select>
-						<option>请选择</option>
+				<td class=td2><select name="year">
+						<option value=''>请选择</option>
+						<?php
+							$now_year = date('Y');
+							for($i=$now_year;$i>$now_year-50;$i--){
+						?>
+						<option value='<?php echo $i;?>'><?php echo $i;?>年</option>
+						<?php }?>
 					</select>
-					年
-					<select>
-						<option>请选择</option>
+					年<script>$("[name=year]").val('<?php echo $birthday[0];?>')</script>
+					<select name="month">
+						<option value=''>请选择</option>
+						<?php
+							for($i=1;$i<13;$i++){
+						?>
+						<option value='<?php echo $i;?>'><?php echo $i;?>月</option>
+						<?php }?>
 					</select>
-					月
-					<select>
-						<option>请选择</option>
-					</select>
+					月<script>$("[name=month]").val('<?php echo $birthday[1];?>')</script>
+					<select name="day">
+						<option value=''>请选择</option>
+					</select><script>show_day('<?php echo $birthday[2];?>');</script>
 					日 </td>
 			</tr>
 		</table>
@@ -231,44 +256,65 @@
 		<table class=r_table>
 			<tr>
 				<td class=td1><span>*</span> 宝宝信息：</td>
-				<td class=td2><input class="radio" type="radio" name="nowstate" checked=checked value="准备怀孕" />
-					准备怀孕
-					<input class="radio" type="radio" name="nowstate" value="预产期" />
-					预产期
-					<input class="radio" type="radio" name="nowstate" value="宝宝生日" />
-					宝宝生日</td>
+				<td class=td2><input class="radio" type="radio" <?php if($member->baby_status==2){?>checked="checked"<?php }?> name="nowstate" value="2" />
+					未怀孕
+					<input class="radio" type="radio" name="nowstate" value="3" <?php if($member->baby_status==3){?>checked="checked"<?php }?>>
+					怀孕中
+					<input class="radio" type="radio" name="nowstate" value="1" <?php if($member->baby_status==1){?>checked="checked"<?php }?>>
+					已经生育</td>
 			</tr>
-			
-			<tr>
+			<tr class="bbs">
+			<?php 
+				$birthday = substr($member->baby_birthday,0,10);
+				$birthday = explode('-',$birthday);
+				if($birthday[1]<10)$birthday[1] = substr($birthday[1],1,1);
+				if($birthday[2]<10)$birthday[2] = substr($birthday[2],1,1);
+			?>
 				<td class=td1>出生日期：</td>
-				<td class=td2><select>
-						<option>请选择</option>
+				<td class=td2><select name="bb_year">
+						<option value=''>请选择</option>
+						<?php
+							$now_year = date('Y');
+							for($i=$now_year+2;$i>$now_year-10;$i--){
+						?>
+						<option value='<?php echo $i;?>'><?php echo $i;?>年</option>
+						<?php }?>
 					</select>
 					年
-					<select>
-						<option>请选择</option>
+					<script>$("[name=bb_year]").val('<?php echo $birthday[0];?>')</script>
+					<select name="bb_month">
+						<option value=''>请选择</option>
+						<?php
+							for($i=1;$i<13;$i++){
+						?>
+						<option value='<?php echo $i;?>'><?php echo $i;?>月</option>
+						<?php }?>
 					</select>
 					月
-					<select>
-						<option>请选择</option>
+					<script>$("[name=bb_month]").val('<?php echo $birthday[1];?>')</script>
+					<select name="bb_day">
+						<option value=''>请选择</option>
 					</select>
+					<script>show_baby_day('<?php echo $birthday[2];?>');</script>
 					日 </td>
 			</tr>
-			<tr>
+			<tr class="bbs">
 				<td class=td1>宝宝姓名：</td>
-				<td class=td2><input type="text" /></td>
+				<td class=td2><input name="baby_name" maxlength="6" value="<?php echo $member->baby_name;?>" type="text" /></td>
 			</tr>
-			<tr>
+			<tr class="bbs">
 				<td class=td1>宝宝性别：</td>
-				<td class=td2><input class="radio" type="radio" name="babysex" checked=checked value="1" />
+				<td class=td2><input class="radio" type="radio" name="babysex" <?php if($member->baby_gender==1){?>checked="checked"<?php }?> value="1" />
 					男
-					<input class="radio" type="radio" name="babysex" value="0" />
+					<input class="radio" type="radio" name="babysex" value="2" <?php if($member->baby_gender==2){?>checked="checked"<?php }?> />
 					女</td>
 			</tr>
 		</table>
 		<div id=r_button>
-			<button></button>
+			<button id="submit"></button>
 		</div>
+		<input type="hidden" name="id" value=<?php echo $id;?>>
+		</form>
 	</div>
 	<div id="bottom">关于我们 - 加入我们 - 友情链接 - 联系我们 - 服务条款 - 隐私保护 - 网站地图</div>
 	<div id="bottom_b">哈哈少儿旗下网站  Copyright ? 1997-2010 HAHA.smg.com All Rights Reserved.</div>
