9497f60b4759ff493e0267ff3679d8cfa8bc0fd2
xiangxiangxx <380252551@qq.com>
2010-07-15 13:34:42 +0800

    xx

--- index.php
+++ index.php
@@ -576,7 +576,7 @@
 				</div>
 			</div>
 		<div id="bottom">关于网趣宝贝 - 加入我们 - 友情链接 - 联系我们 - 服务条款 - 隐私保护 - 网站地图</div>
-		<div id="bottom_b">Copyright © 1997-2010 HAHA.smg.com All Rights Reserved.</div>
+		<div id="bottom_b">Copyright &c 1997-2010 HAHA.smg.com All Rights Reserved.</div>
 	</div>
 	<div id="f_r_pg"></div>
 	</div>
