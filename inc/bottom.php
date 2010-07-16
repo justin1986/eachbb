2ad18cda51b80324aba3327210fa7743230a6e5d
justin <zhanghao@xun-ao.com>
2010-07-16 10:18:08 +0800

    bottom css

--- inc/bottom.php
+++ inc/bottom.php
@@ -1,6 +1,8 @@
-<?php 
-	css_include_once_tag('bottom');
-?>
+<style>
+#bg_hr{width:958px; height:4px; margin-top:25px; margin-left:10px; background:url(/images/class/b_hr.jpg) repeat-x; float:left; display:inline;}
+#bottom{width:980px; margin-top:15px; text-align:center; font-size:12px; color:#666666; float:left; display:inline;}
+#bottom_b{width:980px; margin-top:10px; margin-bottom:50px; text-align:center; font-size:12px; color:#666666; float:left; display:inline;}
+</style>
 <div id="bg_hr"></div>
 <div id="bottom">关于网趣宝贝- 加入我们 - 友情链接 - 联系我们 - 服务条款 - 隐私保护 - 网站地图</div>
 <div id="bottom_b">Copyright &copy; 1997-2010 HAHA.smg.com All Rights Reserved.</div>
