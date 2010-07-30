fa553ce4473e8beb538fbb81a1ebe0bad9b7a4c9
xiangxiangxx <380252551@qq.com>
2010-07-30 15:35:41 +0800

    xx

---
+++ login/_public_logined.php
@@ -0,0 +1,11 @@
+<?php 
+	include_once '../frame.php';
+	$db=get_db();
+	$user = User::current_user();
+	$list=$db->query("SELECT unread_msg_count FROM eachbb_member.member_status where uid=".$user->uid);
+?>
+<div class="xiaoxi" style="margin-top:9px; padding-left:5px; cursor:pointer;  margin-right:5px;">消息<a href="#" style=" color:#FE6E0E;">（<?php echo $list[0]->unread_msg_count;?>）</a></div>
+<div class="exit" style="border-left:2px solid #949494; cursor:pointer;  border-right:2px solid #949494; padding-left:5px; padding-right:5px;">退出</div>
+<div class="xiaoxi" style="padding-left:5px; padding-right:5px; cursor:pointer;  overflow:hidden; text-align: left;">欢迎，<a href="/yard/member.php" style="color:#FE6E0E;"><?php echo $_COOKIE['member_name'];?></a></div>
+<input type="button" id="me_btn" >
+<input type="text" id="me_in" style="height:18px;"/>
