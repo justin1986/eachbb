f63739d86565ef259d6be020dd838368f0d7cb79
xiangxiangxx <380252551@qq.com>
2010-08-02 12:49:19 +0800

    xx

--- yard/index.php
+++ yard/index.php
@@ -28,78 +28,7 @@
 	</div>
 	<div id="content">
 		<div id="c_l">
-			<div id="cll_z">
-				<div class="cll_zz" style="background:url(/images/yard/ffffff.gif) no-repeat;">
-					<div class="cllz_img" style="background:url(/images/yard/l_a.jpg) no-repeat;"></div>
-					<div class="cllz_word">音乐</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_b.jpg) no-repeat;"></div>
-					<div class="cllz_word">转帖</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_c.jpg) no-repeat;"></div>
-					<div class="cllz_word">投票</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_d.jpg) no-repeat;"></div>
-					<div class="cllz_word">说秘密</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_e.jpg) no-repeat;"></div>
-					<div class="cllz_word">真心话</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_f.jpg) no-repeat;"></div>
-					<div class="cllz_word">天天向上</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_g.jpg) no-repeat;"></div>
-					<div class="cllz_word">游戏大厅</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_k.jpg) no-repeat;"></div>
-					<div class="cllz_word">宠物村</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_l.jpg) no-repeat;"></div>
-					<div class="cllz_word">池塘边</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_m.jpg) no-repeat;"></div>
-					<div class="cllz_word">梦幻城</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_p.jpg) no-repeat;"></div>
-					<div class="cllz_word">阳光牧场</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_o.jpg) no-repeat;"></div>
-					<div class="cllz_word">绿光森林</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_q.jpg) no-repeat;"></div>
-					<div class="cllz_word">抢吧</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_r.jpg) no-repeat;"></div>
-					<div class="cllz_word">贺卡</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_s.jpg) no-repeat;"></div>
-					<div class="cllz_word">新手任务</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_t.jpg) no-repeat;"></div>
-					<div class="cllz_word">白小报</div>
-				</div>
-				<div class="cll_zz">
-					<div class="cllz_img" style="background:url(/images/yard/l_u.jpg) no-repeat;"></div>
-					<div class="cllz_word">添加</div>
-				</div>
-				<div id="cll_hr"></div>
-				<a href="#"><img id="cllz_bs" src="/images/yard/l_l_s.jpg" /></a>
-			</div>
+			<?php include('./_yard_left.php');?>
 		</div>
 		<div id="c_ll">
 			<div id="cl_t"></div>
