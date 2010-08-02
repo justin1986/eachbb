b240163287c24d8e28a9c80d97b6eebe09267eda
sauger <sauger@163.com>
2010-08-02 11:11:50 +0800

    fix bugs

--- yard/index.php
+++ yard/index.php
@@ -9,13 +9,6 @@
 		css_include_tag('yard');
 		js_include_tag('yard/yard');
 		$db=get_db();
-<<<<<<< HEAD
-		$user = User::current_user();
-		$info = new table_class('`eachbb_member`.member_status');
-		$info->find_by_uid($user->id);
-		$inof->uid = $user->id;
-=======
->>>>>>> 3d8e56df58563df28d14f7785774eda2bd58fe2b
 	?>
 </head>
 <body>
@@ -168,118 +161,7 @@
 					</div>
 				</div>
 				<div id="r_pho">
-<<<<<<< HEAD
-					<div id="r_img">
-						<div id="r_pto"><img src="<?php echo $news[0]->avatar;?>"></div>
-						<div id="r_bb"><?php echo $news[0]->baby_name;?></div>
-						<div id="r_num">被访问过<?php echo $news[0]->visit_count;?>次</div>
-					</div>
-					<div id="r_geng"> 
-						<div id="r_ge_a">
-							<div id="r_gi_a"></div>
-							<div id="r_gw_a"><a href="#">更换头像</a></div>
-						</div>
-						<div id="r_ge_b">
-							<div id="r_gi_b"></div>
-							<div id="r_gw_b"><a href="/yard/member.php">修改档案</a></div>
-						</div>
-					</div>
-					<div id="r_ge_ge">
-						<div id="r_ge_hr"></div>
-						<div id="r_ge_table">
-							<div class="r_ge_ct">
-								<div class="r_ge_cta"><img src="/images/yard/r_a.jpg"></div>
-								<div class="r_ge_ctb">性别：</div>
-								<div class="r_ge_ctc"><?php echo get_gender($user->gender);?></div>
-							</div>
-							<div class="r_ge_ct">
-								<div class="r_ge_cta"><img src="/images/yard/r_b.jpg"></div>
-								<div class="r_ge_ctb">生日：</div>
-								<div class="r_ge_ctc"><?php echo date('Y-m-d',strtotime($user->birthday)); ?></div>
-							</div>
-							<div class="r_ge_ct">
-								<div class="r_ge_cta"><img src="/images/yard/r_c.jpg"></div>
-								<div class="r_ge_ctb">地址：</div>
-								<div class="r_ge_ctc"><?php echo $user->address;?></div>
-							</div>
-							<div class="r_ge_ct">
-								<div class="r_ge_cta"><img src="/images/yard/r_d.jpg"></div>
-								<div class="r_ge_ctb">金币：</div>
-								<div class="r_ge_ctc"><?php echo $info->score ? $info->score : 0;?></div>
-							</div>
-							<div class="r_ge_ct">
-								<div class="r_ge_cta"><img src="/images/yard/r_e.jpg"></div>
-								<div class="r_ge_ctb">等级：</div>
-								<div class="r_ge_ctc"><?php echo $info->level ? $info->level : 0;?>级</div>
-							</div>
-							<!-- 
-							<div class="r_ge_ct">
-								<div class="r_ge_cta"><img src="/images/yard/r_f.jpg"></div>
-								<div class="r_ge_ctb">最后天数：</div>
-								<div class="r_ge_ctc" style="width:80px;">天</div>
-							</div>
-							 -->
-							<div class="r_ge_ct">
-								<div class="r_ge_cta"><img src="/images/yard/r_g.jpg"></div>
-								<div class="r_ge_ctb">最后登录：</div>
-								<div class="r_ge_ctc" style="width:80px;"><?php echo substr($user->last_login,0,10);?></div>
-							</div>
-						</div>
-						<div id="friend">
-							<div id="friend_a"></div>
-							<div id="friend_b"></div>
-						</div>
-						<div id="pic_r">
-							<div class="pic_pg">
-								<a href="#">
-								<IMG  class="pic_img" src="/images/yard/pho.jpg"/>
-								</a>
-							</div>
-							<div class="pic_pg">
-								<a href="#">
-								<IMG  class="pic_img" src="/images/yard/pho.jpg"/>
-								</a>
-							</div>
-							<div class="pic_pg">
-								<a href="#">
-								<IMG  class="pic_img" src="/images/yard/pho.jpg"/>
-								</a>
-							</div>
-							<div class="pic_pg">
-								<a href="#">
-								<IMG  class="pic_img" src="/images/yard/pho.jpg"/>
-								</a>
-							</div>
-							<div class="pic_pg">
-								<a href="#">
-								<IMG  class="pic_img" src="/images/yard/pho.jpg"/>
-								</a>
-							</div>
-							<div class="pic_pg">
-								<a href="#">
-								<IMG  class="pic_img" src="/images/yard/pho.jpg"/>
-								</a>
-							</div>
-							<div class="pic_pg">
-								<a href="#">
-								<IMG  class="pic_img" src="/images/yard/pho.jpg"/>
-								</a>
-							</div>
-							<div class="pic_pg">
-								<a href="#">
-								<IMG  class="pic_img" src="/images/yard/pho.jpg"/>
-								</a>
-							</div>
-							<div class="pic_pg">
-								<a href="#">
-								<IMG  class="pic_img" src="/images/yard/pho.jpg"/>
-								</a>
-							</div>
-						</div>
-					</div>
-=======
 					<?php include_once(dirname(__FILE__).'/../inc/_yard_right.php'); ?>
->>>>>>> 3d8e56df58563df28d14f7785774eda2bd58fe2b
 				</div>
 			</div>
 			<div id="cc_b"></div>
