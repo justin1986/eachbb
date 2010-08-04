e858961eb3e47546f234943562250772947fafe4
xiangxiangxx <380252551@qq.com>
2010-08-04 15:04:50 +0800

    xx

--- yard/_diary_sub_ajax_post.php
+++ yard/_diary_sub_ajax_post.php
@@ -1,14 +1,14 @@
 <?php
-include_once('../frame.php');
-$db=get_db();
-$user = User::current_user();
-if(!$user)die("请先登录！");
-$title = htmlspecialchars($_POST['title']);
-$content = htmlspecialchars($_POST['content']);
-$created_id = $_POST['created_id'];
-$sql = "insert into eachbb_member.daily(u_id,created_at,last_edit_time,title,content,visit_count,comment_count,category_id)values({$user->id},now(),now(),'$title','$content',0,0,$created_id)";
-if($db->execute($sql))
-echo "发表日志成功！";
-else
-echo "发表日志失败！";
+	include_once('../frame.php');
+	$db=get_db();
+	$user = User::current_user();
+	if(!$user)die("请先登录！");
+	$title = htmlspecialchars($_POST['title']);
+	$content = htmlspecialchars($_POST['content']);
+	$created_id = $_POST['created_id'];
+	$sql = "insert into eachbb_member.daily(u_id,created_at,last_edit_time,title,content,visit_count,comment_count,category_id)values({$user->id},now(),now(),'$title','$content',0,0,$created_id)";
+	if($db->execute($sql))
+		echo "发表日志成功！";
+	else
+		echo "发表日志失败！";
 ?>
