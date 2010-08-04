29c442e0b599657f6d8896f3d3326f9ddae3764e
sauger <sauger@163.com>
2010-08-04 13:46:43 +0800

    fix bugs

--- test.php
+++ test.php
@@ -10,34 +10,10 @@
 		?>
 	</head>
 	<body>
-<<<<<<< HEAD:test.php
-		<?php 
-			var_dump($_FILES[tmp_name]);
+		<?php
+			$a = 1;
+			$b = -2;
+			echo $a + $b;
 		?>
-		<form action="test.php" method="post" enctype="multipart/form-data">
-			<input type="file" name="file" />
-			<input type="submit" value="ok" />
-		</form>
-=======
-		<form action="test.post.php" method="post">
-			<p>
-			title:<input type="text" name="title" value="<?php echo $_POST['post']; ?>"; />
-			</p>
-			<p>
-			content:<textarea rows="" cols="20" name="content"></textarea>
-			</p>
-			<input type="submit" value="ok" />
-		</form>
-		<div>
-			<?php
-				$db = get_db();
-				$result = $db->query("select * from test");
-				if($result === false) die("query fail");
-				
-				
-			?>
-			title:<?php echo $result[0]->title ?>
-		</div>
->>>>>>> 97ddd1853ec547296390bcc94d98ecde1659494c:test.php
 	</body>
 </html>
