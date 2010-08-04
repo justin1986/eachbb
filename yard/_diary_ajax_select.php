5aca588018f440a2ce151541b2c50a1ef0fb3a08
xiangxiangxx <380252551@qq.com>
2010-08-04 15:19:09 +0800

    xx

--- yard/_diary_ajax_select.php
+++ yard/_diary_ajax_select.php
@@ -1,12 +1,12 @@
 <?php 
 include_once('../frame.php');
-$db=get_db();
-$daily_cate=$db->query("SELECT id,name FROM eachbb_member.daily_category d;");
 ?>
 日志分类：
 <select>
 	<option value="0">请选择分类</option>
 	<?php 
+	$db=get_db();
+	$daily_cate=$db->query("SELECT id,name FROM eachbb_member.daily_category d;");
 	foreach ($daily_cate as $variable) {
 		echo "<option value={$variable->id}>{$variable->name}</option>";
 	}
