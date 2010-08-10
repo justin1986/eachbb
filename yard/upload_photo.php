4b6ccf95d9b9822fceaa411aa756e4ead4f4a0ba
xiangxiangxx <380252551@qq.com>
2010-08-10 09:56:52 +0800

    xx

--- yard/upload_photo.php
+++ yard/upload_photo.php
@@ -133,19 +133,22 @@
 			$('#ii_body').css("display","inline");
 		});
 		$('#btn_b_save').click(function(){
-			var photo_b=$('#upload_title').val().trim();
-			var upload_description=$('#upload_description').val().trim();
-			var photo_b=$('#upload_text').val().trim();
-			if(photo_b.length <= 0){
+			var photo_b=$('#upload_title').val();
+			var upload_description=$('#upload_description').val();
+			var photo_b=$('#upload_text').val();
+			if(photo_b.length == 0){
 				alert('请输入相册的名称！');
 				return false;
-			}else if(upload_description.length <= 0){
+			}else if(photo_b.length >= 50){
+				alert('相册的名称必须小于50字！');
+				return false;
+			}else if(upload_description.length = 0){
 				alert('请输入相册的描述！');
 				return false;
-			}else if($("#ulee").val()==''){
+			}else if($("#ulee").val() == ''){
 				alert("请上传相册的封面！");
 				return false;
-			}else if($("#ulee").val()!=''){
+			}else if($("#ulee").val() != ''){
 				var upfile1 = $("#ulee").val();
 				var upload_file_extension=upfile1.substring(upfile1.length-4,upfile1.length);
 				if(upload_file_extension.toLowerCase()!=".png"&&upload_file_extension.toLowerCase()!=".jpg"&&upload_file_extension.toLowerCase()!=".gif"){
@@ -165,6 +168,9 @@
 		$('#ssubmit').click(function(){
 			if($('#name_photo').val() == ''){
 				alert("请输入图片名称！");
+				return false;
+			}else if($('#name_photo').val().length >=50){
+				alert("输入的图片名称必须小于50字!");
 				return false;
 			}else if($("#upfile").val()!=''){
 				var upfile1 = $("#upfile").val();
