5891733a56c401db3f2478bf6e378653deafa408
pingguo <070144451.cjj@163.com>
2010-08-12 14:50:41 +0800

    cc

--- yard/album_list.del.php
+++ yard/album_list.del.php
@@ -6,9 +6,14 @@ if(!$user){
 	}
 $db = get_db();
 $id = $_POST['id'];
-if($db->execute("delete from eachbb_member.album where id=$id")){
-	echo '删除成功！';
-}else{
-	echo '删除失败！';
-}
+$photo_id = $db->query("select id from eachbb_member.photo where u_id=3");
+//$db->execute("delete from eachbb_member.comment where resource_id in $photo_id and resource_type=photo");
+//$db->execute("delete from eachbb_member.comment where resource_id=$photo_id and resource_type=photo");
+$a = implode(",",$photo_id);
+//if($db->execute("delete from eachbb_member.album where id=$id")){
+//	echo '删除成功！';
+//}else{
+//	echo '删除失败！';
+//}
 ?>
+
