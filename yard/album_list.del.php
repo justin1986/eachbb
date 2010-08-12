6fcbad4de9af9bc22c77f68c0826a777b67d8a7d
xiangxiangxx <380252551@qq.com>
2010-08-12 15:47:52 +0800

    xx

--- yard/album_list.del.php
+++ yard/album_list.del.php
@@ -6,14 +6,18 @@ if(!$user){
 	}
 $db = get_db();
 $id = $_POST['id'];
-$photo_id = $db->query("select id from eachbb_member.photo where u_id=3");
-//$db->execute("delete from eachbb_member.comment where resource_id in $photo_id and resource_type=photo");
-//$db->execute("delete from eachbb_member.comment where resource_id=$photo_id and resource_type=photo");
-$a = implode(",",$photo_id);
-//if($db->execute("delete from eachbb_member.album where id=$id")){
-//	echo '删除成功！';
-//}else{
-//	echo '删除失败！';
-//}
+$array = $db->query("select id from eachbb_member.photo where album_id=$id");
+$count=$db->record_count;
+for ($i = 0 ; $i < $count ; $i++){
+	$result =$result.$array[$i]->id.",";
+}
+$result=substr($result,0,-1);
+$db->execute("delete from eachbb_member.comment where resource_id in ($result) and resource_type='photo'");
+$db->execute("delete from eachbb_member.photo where album_id=$id");
+if($db->execute("delete from eachbb_member.album where id=$id")){
+	echo '删除成功！';
+}else{
+	echo '删除失败！';
+}
 ?>
 
