0d98616f3b2c0d95f4ccc3390d46631ba16bed3e
xiangxiangxx <380252551@qq.com>
2010-08-13 15:00:19 +0800

    xx

--- yard/home.php
+++ yard/home.php
@@ -1,13 +1,5 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <html>
-<script  language="Javascript">
-$(function(){
-	if (event.keyCode==116){
-		event.keyCode=0;
-		event.returnValue=false;
-		}
-});
-</script>
 <head>
 	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
 	<title>小院子</title>
