0b5cdbee965a543958496e2f8a4c3b7849854321
sauger <sauger@163.com>
2010-08-24 13:26:33 +0800

    fix bugs

--- inc/bottom.php
+++ inc/bottom.php
@@ -5,4 +5,4 @@
 </style>
 <div id="bg_hr"></div>
 <div id="bottom">关于网趣宝贝- 加入我们 - 友情链接 - 联系我们 - 服务条款 - 隐私保护 - 网站地图</div>
-<div id="bottom_b">Copyright &copy; 1997-2010 HAHA.smg.com All Rights Reserved.</div>
+<div id="bottom_b">Copyright &copy; 1997-2010 www.eachbaby.com All Rights Reserved.</div>
