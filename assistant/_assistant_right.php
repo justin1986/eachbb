c875e476e6c9ffcd953ad69f88a1aaa4a3245a0a
xx <xiangxiang@xiangxiang-xx.(none)>
2010-08-25 16:13:18 +0800

    xx

--- assistant/_assistant_right.php
+++ assistant/_assistant_right.php
@@ -1,4 +1,23 @@
 <!-- 右边 -->
+<?php
+		include_once dirname(__FILE__).'/../frame.php';
+		$user = User::current_user();
+		if(!$user){
+			alert("请您先登录！");
+			redirect('/login/');
+			exit();
+		}
+		function gender($value){
+			if($value){
+				return $value == 1 ? '男' : '女';
+			}else{
+				return "未知";
+			}
+		}
+		function birthday($birthday){
+			echo $birthday;
+		}
+	?>
 <div id="container_recommand">
 <!-- 右边 第一个  个人档案 -->
 	<div id="individual">
@@ -6,15 +25,15 @@
 		<div id="indiv_container">
 			<div id="indiv">
 				<div id="img_pg">
-					<img src="/images/assistant_list/pho.jpg"/>
+					<img src="<?php echo $user->avatar ? $user->avatar : '/images/assistant_list/pho.jpg';?>"/>
 				</div>
 				<div id="indiv_menu">
-					<div id="indiv_name">夜晚不<img src="/images/assistant_list/pg_p.jpg"></div>
-					<div class="indiv_value"><font>来自：</font>黑龙江</div>
-					<div class="indiv_value"><font>性别：</font>女</div>
-					<div class="indiv_value"><font>年龄：</font>1岁</div>
-					<div class="indiv_value" style="color:#37B0AD;"><font>宝宝：</font>忍忍</div>
-					<div class="indiv_value" style="color:#37B0AD;"><font>年龄：</font>8个月</div>
+					<div id="indiv_name"><?php echo $user->true_name;?><img src="/images/assistant_list/pg_p.jpg"></div>
+					<div class="indiv_value"><font>来自：</font><?php echo $user->address;?></div>
+					<div class="indiv_value"><font>性别：</font><?php echo gender($user->gender);?></div>
+					<div class="indiv_value"><font>年龄：</font><?php echo birthday($user->birthday);?>岁</div>
+					<div class="indiv_value" style="color:#37B0AD;"><font>宝宝：</font><?php echo $user->baby_name;?></div>
+					<div class="indiv_value" style="color:#37B0AD;"><font>年龄：</font><?php echo $user->baby_birthday;?>个月</div>
 				</div>
 			</div>
 			<div class="indiv_bottom">
