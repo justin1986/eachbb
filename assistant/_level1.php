f1ecdc5b771fd05f5127a43a810a0a62a3ab5c37
xx <xiangxiang@xiangxiang-xx.(none)>
2010-08-26 15:36:36 +0800

    xx

--- assistant/_level1.php
+++ assistant/_level1.php
@@ -22,7 +22,7 @@ $filter_age = $age ? array($age) : array(-2,-1,1,2,3);
 	foreach ($filter_age as $f_age){
 		$display_category = array();
 		foreach ($sub_cates as $cate){
-			$sql = "select * from eb_assistant where is_adopt=1 and category_id = {$cate->id} and age=$f_age order by priority asc,created_at desc limit 3";
+			$sql = "select * from eb_assistant where is_adopt=1 and category_id = {$cate->id} and age=$f_age order by priority asc,created_at desc limit 6";
 			$items = $db->query($sql);
 			if($items){
 				$item = new DisplayCategory();
@@ -51,7 +51,7 @@ $filter_age = $age ? array($age) : array(-2,-1,1,2,3);
 			<div class="resultt_banner">
 				<a href="list.php?category_id=<?php echo $display->category_id; ?>&age=<?php echo $f_age;?>"><img src="<?php echo $display->category_image;?>"/></a>
 				<?php foreach($display->assistants as $j => $assistant){?>
-				<div class="result_title"<?php if($j==0){?>style="margin-top:10px;"<?php }?>><a href="/assistant/assistant.php?id=<?php echo $assistant[id];?>" title="<?php echo $assistant[title];?>"><?php echo substr($assistant[title],0,21); if(strlen($assistant[title])>21) echo "……";?></a></div>
+				<div class="result_title"<?php if($j==0 || $j == 1){?>style="margin-top:10px;"<?php }?>><a href="/assistant/assistant.php?id=<?php echo $assistant[id];?>" title="<?php echo $assistant[title];?>"><?php echo substr($assistant[title],0,21); if(strlen($assistant[title])>21) echo "…";?></a></div>
 				<?php }?>
 			</div>
 			<div class="cate_more"  style="float:right;"<?php if($k == 0) echo "style='margin-top:54px;'"; ?>><a href="list.php?category_id=<?php echo $display->category_id?>&age=<?php echo $f_age;?>">更多</a></div>
