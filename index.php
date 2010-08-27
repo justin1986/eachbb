1f5abe3e8fa699f9bd628c93a3bfc88e80d7f554
xx <xiangxiang@xiangxiang-xx.(none)>
2010-08-27 11:11:41 +0800

    xx

--- index.php
+++ index.php
@@ -193,8 +193,10 @@
 					?>
 					<div class="course_list" id="course_list_<?php echo $i-1;?>" style="<?php if(1==$i){ echo 'display:inline;';}else{ echo 'display:none;';} ?>">
 					<div class="student_c"<?php $pos="course_tab_head_$i";show_page_pos($pos,'link_d_i');?>>
-						<div class="s_pic_l"><a href="<?php echo $pos_items[$pos]->href;?>"><img src="<?php echo $pos_items[$pos]->image1;?>" border="0"></a></div>
-						<div class="s_pic_r">
+						<div class="s_pic_l">
+							<a href="<?php echo $pos_items[$pos]->href;?>"><img src="<?php echo $pos_items[$pos]->image1;?>" border="0"></a>
+						</div>
+						<div class="s_pic_r" style="width:510px;">
 							<div class="s_word_top"><?php echo_href($pos_items[$pos]->title, $pos_items[$pos]->href);?></div>
 							<div class="s_word_cotent"><?php echo_href($pos_items[$pos]->description, $pos_items[$pos]->href);?></div>
 						</div>
