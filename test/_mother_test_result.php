d92b3bf811e590358ab6633c387d3996d673a499
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-01 15:00:53 +0800

    xx

--- test/_mother_test_result.php
+++ test/_mother_test_result.php
@@ -1,27 +1,25 @@
-<div id="result">
-	<div class="result_box">
-		<div class="content">
+<div id="result" style="width:700px;">
+	<div class="result_box" style="width:700px;">
+		<div class="content" style="width:700px; padding-bottom:5px;">
 			<font>点评：</font>
-			<?php echo $reports['dadongzuo']->description;?>
 		</div>
+		<div class="content" style="width:700px; padding-left:10px; "><?php echo $reports['dadongzuo']->description;?></div>
 	</div>
 	<div id="btn_recommand"><a href="/test/review.php?id=<?php echo $test_id;?>">回顾题目</a></div>
 </div>
-  <div id="c_hr"></div>
-<div id="recommand_container">
+<div id="recommand_container" style="width:700px; margin-left:25px; padding:0px;">
 	<?php 
 		if(!$reports['dadongzuo']->recommands) $reports['dadongzuo']->recommands = array();
-		foreach ($reports['dadongzuo']->recommands as $val){
 	?>
-	<div class="recommand">
-		<div class="recommand_pg_top">特别推荐</div>
-		<div class="recommand_pg_middle">
-			<div class="recommand_pg_son">
+	<div class="rec_list">
+		<div class="recpg_top" style="padding-bottom:5px; font-size:14px;">特别推荐</div>
+		<div class="recommanmiddle" >
+		<?php 	foreach ($reports['dadongzuo']->recommands as $val){?>
+			<div class="rec_son" style="width:300px; height:20px;  margin-left:5px; line-height:20px;  overflow:hidden; font-weight:lighter; font-size:12px; ">
 				<?php echo $val->description; ?>
 			</div>	
-		</div>
-		<div class="recommand_pg_bottom"></div>
-	</div>
-	<?php 
+			<?php 
 		}?>
+		</div>
+	</div>
 </div>
