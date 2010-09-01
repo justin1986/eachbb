1725541ce08285c053b8602f8b46e0014b0b2830
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-01 15:03:27 +0800

    xx

--- test/_mother_test_result.php
+++ test/_mother_test_result.php
@@ -8,13 +8,12 @@
 	<div id="btn_recommand"><a href="/test/review.php?id=<?php echo $test_id;?>">回顾题目</a></div>
 </div>
 <div id="recommand_container" style="width:700px; margin-left:25px; padding:0px;">
-	<?php 
-		if(!$reports['dadongzuo']->recommands) $reports['dadongzuo']->recommands = array();
-	?>
 	<div class="rec_list">
 		<div class="recpg_top" style="padding-bottom:5px; font-size:14px;">特别推荐</div>
 		<div class="recommanmiddle" >
-		<?php 	foreach ($reports['dadongzuo']->recommands as $val){?>
+		<?php
+			if(!$reports['dadongzuo']->recommands) $reports['dadongzuo']->recommands = array(); 
+			foreach ($reports['dadongzuo']->recommands as $val){?>
 			<div class="rec_son" style="width:300px; height:20px;  margin-left:5px; line-height:20px;  overflow:hidden; font-weight:lighter; font-size:12px; ">
 				<?php echo $val->description; ?>
 			</div>	
