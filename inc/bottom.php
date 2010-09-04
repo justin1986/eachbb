1869b83d7661a4863b17cb55439ce35a43a2229c
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-04 22:01:06 +0800

    xx

--- inc/bottom.php
+++ inc/bottom.php
@@ -5,6 +5,6 @@
 </style>
 <div id="bg_hr"></div>
 <div id="bottom">关于网趣宝贝- 加入我们 - 友情链接 - 联系我们 - 服务条款 - 隐私保护 - 网站地图</div>
-<div id="bottom_b">Copyright &copy; 1997-2010 www.eachbaby.com All Rights Reserved.</div>
+<div id="bottom_b">沪ICP备10204500号  1997-2010 www.eachbaby.com All Rights Reserved.</div>
 <script src="/javascript/get_ad.js">
 </script>
