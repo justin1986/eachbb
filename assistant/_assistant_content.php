3f5c6c0253e46694c2250e49a09e1144134c2419
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-06 17:11:04 +0800

    xx

--- assistant/_assistant_content.php
+++ assistant/_assistant_content.php
@@ -1,13 +1,15 @@
 <?php 
-
+$list=$db->query("SELECT id,category_id,title,created_at FROM eb_assistant where is_adopt=1  order by created_at,last_edited_at,click_count desc limit 12");
 ?>
 <div id="hotspot">育儿热点<a href="#">更多&gt;&gt;</a></div>
 <div id="hotspot_container">
 	<div class="kong" style="height:10px;"></div>
-	<?php for($i = 0 ; $i < 12 ; $i++){ ?>
+	<?php for($i = 0 ; $i < 12 ; $i++){
+		$type = $db->query("select name from eb_category  where category_type='assistant' and id=".$list[$i]->category_id);
+		?>
 	<div class="hotspot_pg">
 		<div></div>
-		<a href="#"><font>[知识榜单]</font>&nbsp;sadfasdfa asf</a>
+		<a href="/assistant/assistant.php?id=<?php echo $list[$i]->id;?>" target="_blank"><font>[<?php echo $type[0]->name;?>]</font>&nbsp;<?php echo $list[$i]->title?></a>
 	</div>
 	<?php } ?>
 	<div class="kong" style="height:10px;"></div>
