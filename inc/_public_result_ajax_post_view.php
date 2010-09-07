a4be09f7d46b8bd21dd621b5c384a1b757253b7c
sauger <sauger@163.com>
2010-09-07 10:13:56 +0800

    fix bugs

--- inc/_public_result_ajax_post_view.php
+++ inc/_public_result_ajax_post_view.php
@@ -5,33 +5,23 @@
 	<meta http-equiv=Content-Language content=zh-CN>
 	<?php
 	include_once '../frame.php';
-	include_once '../inc/User.class.php';
-	use_jquery_ui();
-	css_include_tag('top_inc/test_top','colorbox','index');
-	js_include_tag('jquery.colorbox-min');
 	$result = $_GET['result'];
 	$page= $_GET['page'];
 	if(!$page){
 		die("非法操作！");
 	}
+	$db=get_db();
+	$list = $db->query("select title,description from eb_page_pos where page='$page' and name='$result'");
 	?>
 </head>
 <body>
-<?php
-$db=get_db();
-$list = $db->query("select title,description from eb_page_pos where page='$page' and name='$result'");
-?>
-<div class="test_context" style="width:600px;">
+<div style="width:600px;padding:10px;">
 <?php if($list){?>
-	<div class="context_title" style="margin-top:10px; width:600px; ">标题：<?php echo $list[0]->title;?></div>
-	<div class="context_content" style="width:600px;">内容：<br><?php echo  $list[0]->description ; ?></div>
+	<div style="margin-top:10px; margin-bottom:10px; width:600px; ">标题：<?php echo $list[0]->title;?></div>
+	<div style="width:600px;">内容：<br><?php echo  $list[0]->description ; ?></div>
 	<?php }else{?>
 	<div style="width:600px; height:100px; line-height:100px; font-size:30px; font-weight:bold; text-align:center;">内容为空！</div>
 	<?php }?>
 </div>
-</div>
 </body>
-<script type="text/javascript">
-	
-</script>
 </html>
