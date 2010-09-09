44fa02d8d8cd8bbaa45f7127826a25b94cb0055a
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-09 11:40:29 +0800

    xx

--- assistant/_index.php
+++ assistant/_index.php
@@ -79,8 +79,8 @@
 					<div class="dot"></div>
 					<div class="item">
 						<?php 
-						if(mb_strlen($assistant->name.$assistant->title,"utf-8") > 19){?>
-							[<a href="list.php?category_id=<?php echo $assistant->category_id?>" class="a_category_list"><?php echo $assistant->name;?></a>] <a href="assistant.php?id=<?php echo $assistant->id;?>" title="<?php echo $assistant->title?>" target="_blank" ><?php echo mb_substr($assistant->title,0,15-mb_strlen($assistant->name,"utf-8"),"utf-8")."...";?></a>
+						if(mb_strlen($assistant->name.$assistant->title,"utf-8") > 18){?>
+							[<a href="list.php?category_id=<?php echo $assistant->category_id?>" class="a_category_list"><?php echo $assistant->name;?></a>] <a href="assistant.php?id=<?php echo $assistant->id;?>" title="<?php echo $assistant->title?>" target="_blank" ><?php echo mb_substr($assistant->title,0,14-mb_strlen($assistant->name,"utf-8"),"utf-8")."...";?></a>
 							<?php
 							#echo mb_substr("[<a href='list.php?category_id=".$assistant->category_id."' class='a_category_list'>{$assistant->name}</a>] <a href='assistant.php?id={$assistant->id}' title='{$assistant->title}' target='_blank'>{$assistant->title}</a> ",0,20,"utf-8")."..."?>
 						<?php }else{?>
