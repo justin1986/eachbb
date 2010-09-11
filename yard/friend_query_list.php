9ff0540aeddbb71ee6e18ee5c15d554ea4d04065
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-11 12:01:28 +0800

    xx

--- yard/friend_query_list.php
+++ yard/friend_query_list.php
@@ -31,14 +31,14 @@
 </div>
 </body>
 <style>
-#friend_banner{width:850px; height:250px; float:left; display:inline;}
-#friend_title{width:840px; height:30px; padding-left:10px; background:#A8C783; line-height:30px; font-size:14px; font-weight:bold; float:left; display:inline;}
+#friend_banner{width:950px; height:250px; float:left; display:inline;}
+#friend_title{width:940px; height:30px; padding-left:10px; background:#A8C783; line-height:30px; font-size:14px; font-weight:bold; float:left; display:inline;}
 #friend_where{width:840px; height:35px; margin-top:20px; padding-left:10px; line-height:10px; font-size:14px; float:left; display:inline;}
 #friend_btn{width:100px; height:35px; border:0px solid red;  cursor:pointer;  text-align:center; line-height:30px; background:url(/images/assistant_list/btn_1.jpg) no-repeat; float:right; display: inline;}
 #friend_value{width:850px; margin-top:20px; float:left; display:inline;}
 .friend_result_banner{width:850px; float:left; display:inline;}
 .friend_img{width:80px; height:80px; padding:10px;}
-.friend_result_title{width:840px; height:24px; padding-left:10px;  background:#E5F3CD; line-height:24px; float:left; display:inline;}
+.friend_result_title{width:940px; height:24px; padding-left:10px;  background:#E5F3CD; line-height:24px; float:left; display:inline;}
 .friend_img img{width:80px; height:80px; border:0px solid red;}
 .friend_nike_name{width:700px; height:100px; line-height:100px; font-size:14px; float:right; display:inline;}
 .friend_nike_name a{padding-right:70px;float:right; display:inline;} 
