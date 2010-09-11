09bafb3ea2a6439e2f92921e8cdd71b37a2a43f1
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-11 12:59:27 +0800

    xx

--- yard/_query_friend_ajax_post.php
+++ yard/_query_friend_ajax_post.php
@@ -31,7 +31,8 @@ foreach($list as $list){?>
 	<div class="friend_img">
 		<img src="<?php echo $list->avatar ? $list->avatar :"/images/yard_info_img/1.jpg";?>" />
 	</div>
-	<div class="friend_nike_name">用户名:<?php echo $list->name;?><a href="home.php?id=<?php echo $list->id;?>">查看主页</a></div>
+	<div class="friend_nike_name">用户名:<?php echo $list->name;?></div>
+	<div class="friend_a"><a href="home.php?id=<?php echo $list->id;?>">查看主页</a></div>
 </div>
 <?php }}else{
 	echo "<div style='width:800px; height:100px; line-height:100px; text-align:center; font-size:20px; font-weight:bold;'>对不起，查询结果为空！</div>";	
--- yard/friend_query_list.php
+++ yard/friend_query_list.php
@@ -21,9 +21,11 @@
 	<div id="friend_title">查询好友</div>
 	<form action="friend_query_list.php" method="post">
 	<div id="friend_where">
-		用户名：<input type="text" id="nike_name" name="nike_name" />
-		邮箱：<input type="text" id="email" name="email" />
-		地址：<input type="text" id="address" name="address"/>
+		<div id="friend_bt" >
+			用户名：<input type="text" id="nike_name" name="nike_name" />
+			邮箱：<input type="text" id="email" name="email" />
+			地址：<input type="text" id="address" name="address"/>
+		</div>
 		<div id="friend_btn" >查  询</div>
 	</div>
 	</form>
@@ -31,16 +33,18 @@
 </div>
 </body>
 <style>
-#friend_banner{width:950px; height:250px; float:left; display:inline;}
+.friend_a{width:100px; height:100px; line-height:100px; float:right; display:inline;}
+#friend_banner{width:950px; height:250px; overflow:hidden; float:left; display:inline;}
 #friend_title{width:940px; height:30px; padding-left:10px; background:#A8C783; line-height:30px; font-size:14px; font-weight:bold; float:left; display:inline;}
-#friend_where{width:840px; height:35px; margin-top:20px; padding-left:10px; line-height:10px; font-size:14px; float:left; display:inline;}
-#friend_btn{width:100px; height:35px; border:0px solid red;  cursor:pointer;  text-align:center; line-height:30px; background:url(/images/assistant_list/btn_1.jpg) no-repeat; float:right; display: inline;}
+#friend_where{width:940px; height:35px; margin-top:20px; padding-left:10px; line-height:10px; font-size:14px; float:left; display:inline;}
+#friend_btn{width:100px; height:35px; border:0px solid red;  cursor:pointer;  text-align:center; line-height:30px; background:url(/images/assistant_list/btn_1.jpg) no-repeat; float:left; display: inline;}
+#friend_bt{width:800px; height:35px; float:left; display: inline;}
 #friend_value{width:850px; margin-top:20px; float:left; display:inline;}
 .friend_result_banner{width:850px; float:left; display:inline;}
 .friend_img{width:80px; height:80px; padding:10px;}
 .friend_result_title{width:940px; height:24px; padding-left:10px;  background:#E5F3CD; line-height:24px; float:left; display:inline;}
 .friend_img img{width:80px; height:80px; border:0px solid red;}
-.friend_nike_name{width:700px; height:100px; line-height:100px; font-size:14px; float:right; display:inline;}
+.friend_nike_name{width:600px; height:100px; line-height:100px; font-size:14px; float:left; display:inline;}
 .friend_nike_name a{padding-right:70px;float:right; display:inline;} 
 </style>
 </html>
