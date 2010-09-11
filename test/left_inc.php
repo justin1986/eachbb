db19488f91e0986b3b77a6151a48719821a76358
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-11 15:05:19 +0800

    xx

--- test/left_inc.php
+++ test/left_inc.php
@@ -7,8 +7,11 @@
 <div id="c_l">
 	<div id="l_pho"></div>
 	<?php if($user){?>
-	<div id="l_look">
+	<div class="l_look">
 		<a href="/feedback.php?type=test">网趣宝贝<font>意见反馈&nbsp;&nbsp;</font></a>
+	</div>
+	<div class="l_look">
+		<a href="/baby">网趣宝贝<font>我的测评&nbsp;&nbsp;</font></a>
 	</div>
 	<?php }?>
 	<div class="hlc_t"></div>
@@ -59,5 +62,5 @@
 </div>
 <style>
 #l_test{border:0px solid red;}
-#l_look{border:0px solid red;}
+.l_look{border:0px solid red;}
 </style>
