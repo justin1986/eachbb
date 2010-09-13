cf2975e03c28c6f4cae4e7d55a60946a8cc0ce66
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-13 15:08:01 +0800

    xx

--- baby/_baby_view_ajax_post.php
+++ baby/_baby_view_ajax_post.php
@@ -25,7 +25,7 @@
 	$i=0;
 	foreach ($array as $problem){
 		?>
-	<div class="problem_bannerr" <?php if($i % 2 == 0){echo 'background:#D2D8E4;';}?>>
+	<div class="problem_bannerr" <?php if($i % 2 == 0){echo 'style="background:#D2D8E4;"';}?>>
 		<div><?php echo $problem>name; ?></div>
 		<div style="font-size:12px; font-weight:100px;">测评时间：<?php echo $problem->created_at; ?></div>
 		<div style="margin-left:120px; float:left;"><a href="/test/test_result.php?test_id=<?php echo $problem->problem_id;?>">测评报告</a></div>
