7ecc391a07913331b9f1c77932737d2e42159320
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-15 15:24:29 +0800

    xx

--- assistant/_index.php
+++ assistant/_index.php
@@ -81,11 +81,11 @@
 								</div>
 								<div class="assistant_top_pg_c"<?php $pos = "assistant_top_pg_c";show_page_pos($pos,'link')?>>
 									<div class="htct_l"></div>
-									<a href="<?php echo $pos_items[$pos]->href;?>" class="die_assistant">
+<!--									<a href="<?php echo $pos_items[$pos]->href;?>" class="die_assistant">-->
 										<?php $tilte_count =mb_strlen($pos_items[$pos]->title,"utf-8");
 										 echo # $tilte_count >45 ?mb_substr($pos_items[$pos]->title,0,46,"utf-8").'<font style="font-size:12px;">...</font>':
 										 $pos_items[$pos]->title;?>
-									</a>
+<!--									</a>-->
 								</div>
 								<div class="htl_pg_c">
 						</div>
