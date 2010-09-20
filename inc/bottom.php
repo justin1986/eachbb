3b498d59e6e831d6dba96d2f8f5cedfc7c004a39
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-20 12:58:57 +0800

    xx

--- inc/bottom.php
+++ inc/bottom.php
@@ -1,10 +1,24 @@
 <style>
 #bg_hr{width:958px; height:4px; margin-top:25px; margin-left:10px; background:url(/images/class/b_hr.jpg) repeat-x; float:left; display:inline;}
 #bottom{width:980px; margin-top:15px; text-align:center; font-size:12px; color:#666666; float:left; display:inline;}
-#bottom_b{width:980px; margin-top:10px; margin-bottom:50px; text-align:center; font-size:12px; color:#666666; float:left; display:inline;}
+#bottom_b{width:980px; margin-top:10px; margin-bottom:50px; text-align:center; float:left; display:inline;}
+.bottom_a{font-size:12px; color:#666666;  text-decoration: none;}
 </style>
+	<?php 
+			include_once(dirname(__FILE__).'./../frame.php');
+			use_jquery_ui();
+			init_page_items('bottom');
+	?>
 <div id="bg_hr"></div>
-<div id="bottom">关于网趣宝贝- 加入我们 - 友情链接 - 联系我们 - 服务条款 - 隐私保护 - 网站地图</div>
+<div id="bottom">
+	<a class="bottom_a"<?php $pos="bottom_a";show_page_pos($pos,'link');?> href="<?php echo $pos_items[$pos]->href;?>" ><?php echo $pos_items[$pos]->title; ?>-</a> 
+	<a class="bottom_a"<?php $pos="bottom_b";show_page_pos($pos,'link');?> href="<?php echo $pos_items[$pos]->href;?>" ><?php echo $pos_items[$pos]->title; ?>-</a>
+	<a class="bottom_a"<?php $pos="bottom_c";show_page_pos($pos,'link');?> href="<?php echo $pos_items[$pos]->href;?>" ><?php echo $pos_items[$pos]->title; ?>-</a>
+	<a class="bottom_a"<?php $pos="bottom_d";show_page_pos($pos,'link');?> href="<?php echo $pos_items[$pos]->href;?>" ><?php echo $pos_items[$pos]->title; ?>-</a>
+	<a class="bottom_a"<?php $pos="bottom_e";show_page_pos($pos,'link');?> href="<?php echo $pos_items[$pos]->href;?>" ><?php echo $pos_items[$pos]->title; ?>-</a>
+	<a class="bottom_a"<?php $pos="bottom_f";show_page_pos($pos,'link');?> href="<?php echo $pos_items[$pos]->href;?>" ><?php echo $pos_items[$pos]->title; ?>-</a>
+	<a class="bottom_a"<?php $pos="bottom_g";show_page_pos($pos,'link');?> href="<?php echo $pos_items[$pos]->href;?>" ><?php echo $pos_items[$pos]->title; ?></a>
+</div>
 <div id="bottom_b">沪ICP备10204500号  <script src="http://s17.cnzz.com/stat.php?id=2415758&web_id=2415758&show=pic1" language="JavaScript"></script></div>
 <script src="/javascript/get_ad.js">
 </script>
