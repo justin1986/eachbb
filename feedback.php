8f9a6aee75761d3fa631f75430fa32c456efbcb7
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-20 11:46:14 +0800

    xx

--- feedback.php
+++ feedback.php
@@ -47,7 +47,7 @@
 				<div id="h_title"<?php $pos="feedback_title1";show_page_pos($pos,'link')?>><a href="<?php echo $pos_items[$pos]->href;?>" target="_blank"><?php echo $pos_items[$pos]->title;?></a></div>
 				<div id="c_title"<?php $pos="feedback_title2";show_page_pos($pos,'link');?>><a href="<?php echo $pos_items[$pos]->href;?>" target="_blank"><?php echo $pos_items[$pos]->title;?></a></div>
 				<div id="cc_hr"></div>
-				<div id="cc_c"<?php $pos="feedback_desc";show_page_pos($pos,'link_desc');?>><?php echo $pos_items[$pos]->description;?></div>
+				<div id="cc_c"<?php $pos="feedback_desc";show_page_pos($pos,'link_desc');?>><?php echo $pos_items[$pos]->description;?>&nbsp;</div>
 				<?php 
 				$user = User::current_user();
 				if(!$user){
