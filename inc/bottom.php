17e6b8185c08a26e0400e562bca78e7e1c8e8837
xx <xiangxiang@xiangxiang-xx.(none)>
2010-09-27 13:51:30 +0800

    xx

--- inc/bottom.php
+++ inc/bottom.php
@@ -9,7 +9,7 @@
 	?>
 <div id="bg_hr"></div>
 <div id="bottom">
-	<?php for($i=0 ; $i< 7 ; $i++){
+	<?php for($i=0 ; $i< 6 ; $i++){
 		$list = $db->query("SELECT * FROM eb_page_pos where page='index' and name like 'bottom_link_$i' limit 7");
 		?>
 	<a class="bottom_a" <?php $pos="bottom_link_".$i;show_page_pos($pos,'link');?> href="<?php echo $list[0]->href;?>">
